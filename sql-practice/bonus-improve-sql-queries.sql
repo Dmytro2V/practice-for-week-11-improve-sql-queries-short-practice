381db5322904b80ed4cc3ca9788f037ed4fd3b24
Dmytro2V <op2vin@gmail.com>
2023-03-08 01:22:59 +0200

    done with main and bonus bench practice

--- sql-practice/bonus-improve-sql-queries.sql
+++ sql-practice/bonus-improve-sql-queries.sql
@@ -3,10 +3,15 @@
 ----------
 -- Query: Find a count of `toys` records that have a price greater than
     -- 55 and belong to a cat that has the color "Olive".
-
+    
     -- Your code here
+    select count(*) FROM cat_toys 
+        WHERE cat_id IN (SELECT id FROM cats WHERE color = 'Olive') 
+        AND toy_id IN (SELECT id FROM toys WHERE price > 55)
+    ;
 
 -- Paste your results below (as a comment):
+    -- 215
 
 
 
@@ -17,16 +22,27 @@
 -- Query:
 
     -- Your code here
+    EXPLAIN QUERY PLAN     select count(*) FROM cat_toys 
+        WHERE cat_id IN (SELECT id FROM cats WHERE color = 'Olive') 
+        AND toy_id IN (SELECT id FROM toys WHERE price > 55)
+    ;
 
 -- Paste your results below (as a comment):
+    --SEARCH TABLE cat_toys USING INDEX idx_cat_toys_toy_id (toy_id=?)
+    --LIST SUBQUERY 2
+      --SCAN TABLE toys
+    --LIST SUBQUERY 1
+      --SCAN TABLE cats
 
 
 -- What do your results mean?
 
     -- Was this a SEARCH or SCAN?
+      -- search on mix table, scan on sides
 
 
     -- What does that mean?
+        -- indexes only on mixed, ordered same
 
 
 
@@ -37,9 +53,14 @@
 -- Query (to be used in the sqlite CLI):
 
     -- Your code here
-
+.timer on
+    select count(*) FROM cat_toys 
+        WHERE cat_id IN (SELECT id FROM cats WHERE color = 'Olive') 
+        AND toy_id IN (SELECT id FROM toys WHERE price > 55)
+    ;
+.timer off
 -- Paste your results below (as a comment):
-
+    -- Run Time: real 0.008 user 0.007621 sys 0.000000
 
 
 
@@ -50,16 +71,28 @@
 -- Create index:
 
     -- Your code here
-
+    CREATE INDEX idx_cats_color ON cats(color);
+    CREATE INDEX idx_toys_price ON toys(price);
 -- Analyze Query:
     -- Your code here
 
+    EXPLAIN QUERY PLAN     select count(*) FROM cat_toys 
+        WHERE cat_id IN (SELECT id FROM cats WHERE color = 'Olive') 
+        AND toy_id IN (SELECT id FROM toys WHERE price > 55)
+    ;
 -- Paste your results below (as a comment):
+    -- QUERY PLAN
+     --SEARCH TABLE cat_toys USING INDEX idx_cat_toys_toy_id (toy_id=?)
+       --LIST SUBQUERY 2
+     --SEARCH TABLE toys USING COVERING INDEX idx_toys_price (price>?)
+       --LIST SUBQUERY 1
+   --SEARCH TABLE cats USING COVERING INDEX idx_cats_color (color=?)
 
 
 -- Analyze Results:
 
     -- Is the new index being applied in this query?
+        -- yes
 
 
 
@@ -70,21 +103,45 @@
 -- Query (to be used in the sqlite CLI):
 
     -- Your code here
+.timer on
+    select count(*) FROM cat_toys 
+        WHERE cat_id IN (SELECT id FROM cats WHERE color = 'Olive') 
+        AND toy_id IN (SELECT id FROM toys WHERE price > 55)
+    ;
+.timer off
 
 -- Paste your results below (as a comment):
+    -- Run Time: real 0.006 user 0.006091 sys 0.000000
 
 
 -- Analyze Results:
     -- Are you still getting the correct query results?
-
+        -- yes
 
     -- Did the execution time improve (decrease)?
-
+        -- a bit
 
     -- Do you see any other opportunities for making this query more efficient?
-
+        -- can try use join
 
 
 ---------------------------------
 -- Notes From Further Exploration
 ---------------------------------
+EXPLAIN QUERY PLAN select count(*) FROM 
+            cat_toys JOIN cats ON (cats.id = cat_id)
+            JOIN toys ON (toys.id = toy_id)
+        WHERE  cats.color = 'Olive'
+        AND  price > 55
+    ;
+
+.timer on
+    select count(*) FROM 
+            cat_toys JOIN cats ON (cats.id = cat_id)
+            JOIN toys ON (toys.id = toy_id)
+        WHERE  cats.color = 'Olive'
+        AND  price > 55
+    ;
+.timer off
+-- Run Time: real 0.006 user 0.005391 sys 0.000000
+-- a bit better
